c4dcfa7d187859878c6e5ad25542ebe1efea62d0
Meshari <msh.95@hotmail.com>
2021-04-01 23:51:58 +1300

    add to app.test

--- src/App.test.js
+++ src/App.test.js
@@ -6,3 +6,10 @@ import React from 'react';
  ReactDOM.render(<App />, div);
  ReactDOM.unmountComponentAtNode(div);
  });
+
+ describe('Addition', () => {
+  it('knows that 2 and 2 make 4', () => {
+  expect(2 + 2).toBe(4);
+  });
+  });
+  
